6d79d6dddad7e058b26793a4678e9c6e9ac86ca9
Davi Saldanha <davisaldanha.ti@gmail.com>
2024-07-25 08:25:57 -0300

    Update class MainActivity

--- app/src/main/java/senai/examplelistview/MainActivity.kt
+++ app/src/main/java/senai/examplelistview/MainActivity.kt
@@ -18,30 +18,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val listUsers = ArrayList<User>()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listUsers)
+        val database = MyDatabaseHelper(this)
+        var listUsers = database.readUsers()
+        var adapter = setAdapter(listUsers)
 
         binding.listaItens.adapter = adapter
 
         binding.listaItens.setOnItemClickListener { parent, view, position, id ->
-            binding.username.setText(listUsers.get(position).username)
+            binding.id.text = listUsers.get(position).id.toString()
+            binding.nome.setText(listUsers.get(position).nome)
+            binding.email.setText(listUsers.get(position).email)
+            binding.telefone.setText(listUsers.get(position).telefone)
             binding.password.setText(listUsers.get(position).password)
             this.position = position
         }
 
         //Atualizar o evento de cadastrar verificando se o usuário já não está cadastrado
         // na lista
+
         binding.cadastrar.setOnClickListener {
-            if(!binding.username.text.toString().trim().isEmpty() && !binding.password.text.toString().trim().isEmpty()){
-                listUsers.add(
-                    User(
-                        binding.username.text.toString(),
-                        binding.password.text.toString()
-                    )
+
+            if(!binding.nome.text.toString().trim().isEmpty() &&
+                    !binding.password.text.toString().trim().isEmpty() &&
+                        !binding.telefone.text.toString().trim().isEmpty() &&
+                            !binding.email.text.toString().trim().isEmpty()){
+
+                val value = database.insertUser(
+                    binding.nome.text.toString(),
+                    binding.password.text.toString(),
+                    binding.email.text.toString(),
+                    binding.telefone.text.toString()
                 )
-                adapter.notifyDataSetChanged()
-                binding.username.setText("")
-                binding.password.setText("")
+
+                if(value > -1){
+                    Snackbar.make(
+                        binding.root,
+                        "Usuário " + binding.nome.text.toString() + " cadastrado com sucesso!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
+                listUsers = database.readUsers()
+                adapter = setAdapter(listUsers)
+                binding.listaItens.adapter = adapter
+                //adapter.notifyDataSetChanged()
+                limparCampos()
             }else{
                 Snackbar.make(binding.root, "ERRO! Campos Vazios!", Snackbar.LENGTH_LONG)
                     .setBackgroundTint(resources.getColor(com.google.android.material.R.color.design_dark_default_color_background))
@@ -52,19 +73,54 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.atualizar.setOnClickListener{
-            if (position >= 0){
-                listUsers.get(position).username = binding.username.text.toString().trim()
-                listUsers.get(position).password = binding.password.text.toString().trim()
 
-                adapter.notifyDataSetChanged()
+            var id = binding.id.text.toString().toInt()
+            var nome = binding.nome.text.toString()
+            var telefone = binding.telefone.text.toString()
+            var email = binding.email.text.toString()
+            var senha = binding.password.text.toString()
 
-                binding.username.setText("")
-                binding.password.setText("")
+            var user: User = User(id, nome, senha, email, telefone)
 
-                position = -1
-            }
+            database.updateUser(user)
+
+            listUsers = database.readUsers()
+            adapter = setAdapter(listUsers)
+            binding.listaItens.adapter = adapter
+            limparCampos()
         }
 
         //Criar o evento para excluir usuários
+
+        binding.excluir.setOnClickListener {
+            val value = database.deleteUser(binding.id.text.toString().toInt())
+
+            if (value != 0){
+                Snackbar.make(
+                    binding.root,
+                    "Usuário " + binding.nome.text.toString() + " excluído com sucesso!",
+                    Snackbar.LENGTH_SHORT
+                    ).show()
+            }
+
+            listUsers = database.readUsers()
+            adapter = setAdapter(listUsers)
+            binding.listaItens.adapter = adapter
+            limparCampos()
+        }
+
+    }
+
+    fun limparCampos(){
+        binding.id.text = ""
+        binding.nome.setText("")
+        binding.password.setText("")
+        binding.email.setText("")
+        binding.telefone.setText("")
+    }
+
+
+    fun setAdapter(array: List<User>): ArrayAdapter<User>{
+        return ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
     }
 }
--- app/src/main/java/senai/examplelistview/User.kt
+++ app/src/main/java/senai/examplelistview/User.kt
@@ -1,9 +1,9 @@
 package senai.examplelistview
 
-class User(var Nome: String, var password: String, var email: String, var telefone:String){
+class User(var id: Int, var nome: String, var password: String, var email: String, var telefone:String){
 
 
     override fun toString(): String {
-        return this.Nome
+        return this.nome
     }
 }
